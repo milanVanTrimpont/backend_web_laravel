2c88e85c986cf7e61c9c84d9afa7ce39047e5728
milanvt <milan.van.trimpont@student.ehb.be>
2025-01-13 00:00:22 +0100

    profielen button toegevoegd aan de header

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,6 +15,9 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    <x-nav-link :href="route('profielen')" :active="request()->routeIs('profielen')">
+                        {{ __('Profielen') }}
+                    </x-nav-link>
                 </div>
             </div>
 
