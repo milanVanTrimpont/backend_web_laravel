5f7ef7e8c6492738b5bd18efcc8bb323dca7c624
milanvt <milan.van.trimpont@student.ehb.be>
2025-01-13 19:58:32 +0100

    dashboard voor admins toegevoegd met middleware

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\NieuwsItemController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 
@@ -28,3 +29,7 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+Route::get('admin/dashboard', [HomeController::class, 'index'])->
+middleware(['auth','admin']);
+
